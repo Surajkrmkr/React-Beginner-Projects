5bc0085e82dc106479edcfd83e1450ad4c7d41c6
Suraj Karmakar <53361745+Surajkrmkr@users.noreply.github.com>
2021-11-17 19:56:00 +0530

    new project added

---
+++ 5-menu-app/src/components/Container.js
@@ -0,0 +1,12 @@
+import data from './data';
+import { useState } from 'react';
+
+const Container = () => {
+    return (
+        <div className="container">
+            
+        </div>
+    );
+}
+
+export default Container;
